feb38d750001af2acde98d959cb67e4a798ee69b
Esther Choi <81777226+CatGirl5773@users.noreply.github.com>
2021-08-01 13:37:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
     video.position(400, 400);
 
-    canvas = createCanvas(550, 500);
+    canvas = createCanvas(550, 550);
     canvas.center();
 
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -12,6 +16,10 @@ function setup() {
 
 function draw() {
     background('#fec8c1');
+    fill('#FFFFFF');
+    textSize(difference);
+    text("Esther", 10, 325);
+    document.getElementById("display_font_size").innerHTML = difference + "px";
 }
 
 function modelLoaded() {
@@ -21,5 +29,9 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("Left Wrist X = " + leftWristX + ", Right Wrist X = " + rightWristX + ", Difference = " + difference);
     }
 }
